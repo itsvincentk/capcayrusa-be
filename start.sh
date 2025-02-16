9d684434b59ff7d00cf3db9f673073543c7687c4
Vincent <itsvincentk56@gmail.com>
2025-02-17 02:21:58 +0700

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-pip install -r requirements.txt  # Manually install dependencies
-gunicorn -w 1 -b 0.0.0.0:5000 app:app  # Start the Flask app
+export PATH="/usr/local/bin:/usr/bin:/bin"
+
+# Ensure pip is installed
+python3 -m ensurepip --default-pip
+python3 -m pip install --upgrade pip setuptools wheel
+
+# Install dependencies from requirements.txt
+python3 -m pip install -r requirements.txt
+
+# Start the Flask app with Gunicorn
+gunicorn -w 1 -b 0.0.0.0:5000 app:app
